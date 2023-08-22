bc8395999ca8f36600f2cc5da66a0d2e1ec4a58d
g* <d5e90a9d-b43b-4552-bac7-725497ef58a2@slmail.me>
2023-08-22 23:05:21 +0200

    Updating tracker code (fix to domain title issue in analytics).

--- tracking/matomo.js
+++ tracking/matomo.js
@@ -1,15 +1,18 @@
 var _paq = window._paq = window._paq || [];
 
-_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-_paq.push(["setCookieDomain", "*.gugulet.hu"]);
-_paq.push(["setDomains", ["*.gugulet.hu"]]);
+_paq.push(['setCookieDomain', '*.gugulet.hu']);
+_paq.push(['setDomains', '*.gugulet.hu']);
+_paq.push(['setDocumentTitle', document.domain + "/" + document.title]);
+_paq.push(['enableHeartBeatTimer', 3]);
+_paq.push(['trackVisibleContentImpressions', true, 500]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
-_paq.push(['enableHeartBeatTimer']);
+
 (function () {
         var u = "https://analytics.gugulet.hu/";
         _paq.push(['setTrackerUrl', u + 'matomo.php']);
         _paq.push(['setSiteId', '1']);
+
         var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
         g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
 })();
