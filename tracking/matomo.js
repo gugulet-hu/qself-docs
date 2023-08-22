13a62c4217289f025542489d46b92ac3be636f20
g* <d5e90a9d-b43b-4552-bac7-725497ef58a2@slmail.me>
2023-08-23 00:14:43 +0200

    Reorder tracking code.

--- tracking/matomo.js
+++ tracking/matomo.js
@@ -1,17 +1,15 @@
 var _paq = window._paq = window._paq || [];
 
-_paq.push(['enableHeartBeatTimer', 3]);
-_paq.push(['trackVisibleContentImpressions', true, 500]);
+_paq.push(["setDocumentTitle", "qself.gugulet.hu/" + document.title]);
+_paq.push(["setCookieDomain", "*.gugulet.hu"]);
+_paq.push(["setDomains", ["*.gugulet.hu"]]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
-
+_paq.push(['enableHeartBeatTimer']);
 (function () {
         var u = "https://analytics.gugulet.hu/";
         _paq.push(['setTrackerUrl', u + 'matomo.php']);
         _paq.push(['setSiteId', '1']);
-        _paq.push(['setCookieDomain', '*.gugulet.hu']);
-        _paq.push(['setDomains', '*.gugulet.hu']);
-        _paq.push(['setDocumentTitle', "q.self.gugulet.hu/" + document.title]);
         var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
         g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
 })();
