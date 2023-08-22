2f029d8d75b64b5640bda1a90c3926366941bc7b
g* <d5e90a9d-b43b-4552-bac7-725497ef58a2@slmail.me>
2023-08-23 00:09:32 +0200

    Reorder tracking code.

--- tracking/matomo.js
+++ tracking/matomo.js
@@ -1,7 +1,5 @@
 var _paq = window._paq = window._paq || [];
 
-// _paq.push(['setCookieDomain', '*.gugulet.hu']);
-// _paq.push(['setDomains', '*.gugulet.hu']);
 _paq.push(['setDocumentTitle', "q.self.gugulet.hu/" + document.title]);
 _paq.push(['enableHeartBeatTimer', 3]);
 _paq.push(['trackVisibleContentImpressions', true, 500]);
@@ -12,7 +10,8 @@ _paq.push(['enableLinkTracking']);
         var u = "https://analytics.gugulet.hu/";
         _paq.push(['setTrackerUrl', u + 'matomo.php']);
         _paq.push(['setSiteId', '1']);
-
+        _paq.push(['setCookieDomain', '*.gugulet.hu']);
+        _paq.push(['setDomains', '*.gugulet.hu']);
         var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
         g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
 })();
