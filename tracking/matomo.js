d77558ec71979a59c174512baa326e4c374efc26
g* <d5e90a9d-b43b-4552-bac7-725497ef58a2@slmail.me>
2023-11-09 22:10:02 +0100

    Revert tracking code.

--- tracking/matomo.js
+++ tracking/matomo.js
@@ -8,10 +8,11 @@ _paq.push(["setDomains", ["*.gugulet.hu", "*.qself.gugulet.hu"]]);
 _paq.push(["enableCrossDomainLinking"]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
+
 (function () {
         var u = "https://analytics.gugulet.hu/";
         _paq.push(['setTrackerUrl', u + 'matomo.php']);
-        _paq.push(['setSiteId', '3']);
+        _paq.push(['setSiteId', '1']);
         var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
         g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
 })();
